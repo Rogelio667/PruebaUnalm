3ac76fb9210d1bc35062e909a8f4850812e3403d
Rogelio667 <112987958+Rogelio667@users.noreply.github.com>
2023-10-16 12:04:47 -0500

    primer commit

---
+++ src/main/java/com/mycompany/pruebaunalm/PruebaUnalm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaunalm;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class PruebaUnalm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
